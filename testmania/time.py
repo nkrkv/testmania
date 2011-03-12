c921cfb392e27208d4f03b2fd5c618082624c585
Victor Nakoryakov <nail.xx@gmail.com>
2011-03-12 21:02:40 +0300

    fixed typo in assert_just_now.__doc__

--- testmania/time.py
+++ testmania/time.py
@@ -9,7 +9,7 @@ def assert_just_now(time_value, msg=None, tolerance=1):
     It's handy to test timestamp values like ``created_at`` or ``updated_at``.
 
     :param time_value: should be a :py:class:`~datetime.datetime` object.
-    :param tolerance, int or float: specifies acceptable deviation
+    :param int|float tolerance: specifies acceptable deviation
         of `time_value` in seconds.
     """
     delta = datetime.datetime.now() - time_value
